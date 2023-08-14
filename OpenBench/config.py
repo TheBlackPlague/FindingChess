67441237c311d1875cdfd561864c6430d982fa15
SzilBalazs <szilprogramming@gmail.com>
2023-08-14 16:51:27 +0200

    Update WhiteCore benchmark speed again

--- OpenBench/config.py
+++ OpenBench/config.py
@@ -87,7 +87,7 @@ OPENBENCH_CONFIG = {
         },
 
         'WhiteCore' : {
-            'nps': 2200000,
+            'nps': 1300000,
             'base': 'master',
             'book': 'UHO_XXL_+0.90_+1.19.epd',
             'bounds': '[0.00, 5.00]',
